e040cefed47a5d8b328d75faade55de044668b9b
Marat <maratka24@gmail.com>
2016-03-14 23:54:10 +0300

    extract ShopModel to class; form for adding shop goods

--- ShopClient/MainWindow.xaml.cs
+++ ShopClient/MainWindow.xaml.cs
@@ -1,83 +1,14 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.ComponentModel;
 using System.Text.RegularExpressions;
+using System.Collections.Specialized;
 using System.Threading;
 
 namespace ShopClient
 {
-    [Serializable]
-    public class ShopData
-    {
-        public String Name { get; set; }
-        public String Address { get; set; }
-        public String PhoneNumber { get; set; }
-        public String Email { get; set; }
-
-        public int Token { get; set; }
-        /// <summary>
-        /// Listener Port на клиенте
-        /// </summary>
-        public int Port { get; set; }
-    }
-
-    public class ShopModel : ShopData, IDataErrorInfo
-    {
-        public String Error
-        {
-            get { return null; }
-        }
-
-        public String this[String controlName]
-        {
-            get
-            {
-                String result = String.Empty;
-
-                if (controlName == "Name" && String.IsNullOrEmpty(Name))
-                {
-                    result = "Имя фирмы обязательно к заполнению";
-                }
-                else if (controlName == "Address" && String.IsNullOrEmpty(Address))
-                {
-                    result = "Адрес обязателен к заполнению";
-                }
-                else if (controlName == "PhoneNumber")
-                {
-                    if (String.IsNullOrEmpty(PhoneNumber))
-                    {
-                        result = "Телефон обязателен к заполнению";
-                    }
-                    else
-                    {
-                        Regex phoneRegEx = new Regex(@"^\d{1}\(\d{3}\)\d{3}-\d{2}-\d{2}$");
-
-                        if (!phoneRegEx.IsMatch(PhoneNumber))
-                            result = "Введите телефон полностью";
-                    }
-                }
-                else if (controlName == "Email")
-                {
-                    if (String.IsNullOrEmpty(Email))
-                    {
-                        result = "Email обязателен к заполнению";
-                    }
-                    else
-                    {
-                        Regex simpleMailRegEx = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
-
-                        if (!simpleMailRegEx.IsMatch(Email))
-                            result = "Email введён некорректно, пример: example@service.com";
-                    }
-                }
-
-                return result;
-            }
-        }
-    }
-
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
@@ -86,37 +17,54 @@ namespace ShopClient
         ShopModel _shop = new ShopModel();
         int _errorsCount = 0;
 
+        Model.GoodModel _goodModel;
+
         //TODO: config
         ServerInteraction _interaction = new ServerInteraction(9316);
 
         //первое сообщение - xml с mappingoм, далее сообщения с товарами
         bool _isShopMapped = false;
-        bool _isProcessing = false;
 
         public MainWindow()
         {
             InitializeComponent();
-            ShopDataGrid.DataContext = _shop;
+
+            ShopPanel.DataContext = _shop;
+
+            _goodModel = new Model.GoodModel();
+            GoodsList.DataContext = _goodModel;
+            _goodModel.Goods.CollectionChanged += Goods_CollectionChanged;
+
+            ProcessingReceivedFiles();
         }
 
-        void Send_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        private void Goods_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
-            e.CanExecute = _errorsCount == 0;
-            e.Handled = true;
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                
+            }
         }
 
         void ProcessingReceivedFiles()
         {
             Thread receiveThread = new Thread(() =>
             {
-                _isProcessing = true;
+                //TODO: отлавливать выход из ф-ции и перезапускать её
                 _interaction.ReceiveXmlMessages();
-                _isProcessing = false;
             });
 
             receiveThread.IsBackground = true;
             receiveThread.Start();
-        } 
+        }
+
+        #region Shop Commands
+
+        void Send_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _errorsCount == 0;
+            e.Handled = true;
+        }
 
         void Send_Executed(object sender, ExecutedRoutedEventArgs e)
         {
@@ -140,11 +88,10 @@ namespace ShopClient
 
             MessageBox.Show(sendResult);
 
-            if (!_isProcessing)
-                ProcessingReceivedFiles();
-
             e.Handled = true;
         }
+
+        #endregion
 
         void Validation_Error(object sender, ValidationErrorEventArgs e)
         {
@@ -152,6 +99,12 @@ namespace ShopClient
                 _errorsCount++;
             else
                 _errorsCount--;
+        }
+
+        void QuantityValidation(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
---
+++ ShopClient/ShopModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace ShopClient
+{
+    [Serializable]
+    public class ShopData
+    {
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public String PhoneNumber { get; set; }
+        public String Email { get; set; }
+
+        public int Token { get; set; }
+        /// <summary>
+        /// Listener Port на клиенте
+        /// </summary>
+        public int Port { get; set; }
+    }
+
+    public class ShopModel : ShopData, IDataErrorInfo
+    {
+        public String Error
+        {
+            get { return null; }
+        }
+
+        public String this[String controlName]
+        {
+            get
+            {
+                String result = String.Empty;
+
+                if (controlName == "Name" && String.IsNullOrEmpty(Name))
+                {
+                    result = "Имя фирмы обязательно к заполнению";
+                }
+                else if (controlName == "Address" && String.IsNullOrEmpty(Address))
+                {
+                    result = "Адрес обязателен к заполнению";
+                }
+                else if (controlName == "PhoneNumber")
+                {
+                    if (String.IsNullOrEmpty(PhoneNumber))
+                    {
+                        result = "Телефон обязателен к заполнению";
+                    }
+                    else
+                    {
+                        Regex phoneRegEx = new Regex(@"^\d{1}\(\d{3}\)\d{3}-\d{2}-\d{2}$");
+
+                        if (!phoneRegEx.IsMatch(PhoneNumber))
+                            result = "Введите телефон полностью";
+                    }
+                }
+                else if (controlName == "Email")
+                {
+                    if (String.IsNullOrEmpty(Email))
+                    {
+                        result = "Email обязателен к заполнению";
+                    }
+                    else
+                    {
+                        Regex simpleMailRegEx = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+                        if (!simpleMailRegEx.IsMatch(Email))
+                            result = "Email введён некорректно, пример: example@service.com";
+                    }
+                }
+
+                return result;
+            }
+        }
+    }
+}
